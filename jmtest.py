43bb54043e9211adb5e287842fc84d09fefb7081
markbejosano <mbejosano@west.com>
2018-07-26 15:48:35 -0500

    added foldename

--- jmtest.py
+++ jmtest.py
@@ -1,16 +1,16 @@
 import os
-import os.path
+if len(sys.argv)==0:
+		print("please pass folder name as argument")
+		return
+foldername=sys.argv[1]
+y = os.path.abspath(foldername+"/HTTPRequest.jmx")
+z = y.replace('\\','\\\\') 
 
-check_file = os.path.isfile("HTTPRequest.jmx")
-print check_file
-check_file = str(check_file)
-if check_file=="True":
-  y = os.path.abspath("./HTTPRequest.jmx")
-  z = y.replace('\\','\\\\') 
+initial_path = "jmeter -Jjmeter.save.saveservice.output_format=xml -n -t"
+command = "-l HTTPRequest.jtl"
 
-  initial_path = "jmeter -Jjmeter.save.saveservice.output_format=xml -n -t"
-  command = "-l HTTPRequest.jtl"
 
-  final = initial_path + " " + z + " " + command
+final = initial_path + " " + z + " " + command
 
-  os.system(final)
+
+os.system(final)
